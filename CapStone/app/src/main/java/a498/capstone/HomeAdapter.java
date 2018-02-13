2a37c1062da40767e6a982303a96b4afb5333e4c
EricPrits <14edp@queensu.ca>
2018-02-12 23:44:56 -0500

    Made a new adapter for list view on home page
    New homeadpter is slightly altered version on detailed adapter

---
+++ CapStone/app/src/main/java/a498/capstone/HomeAdapter.java
@@ -0,0 +1,44 @@
+package a498.capstone;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * This class is a Custom Adapter used to display detailed receipt data in a list view
+ *
+ * Created by ericprits on 2018-02-01.
+ */
+
+public class HomeAdapter extends ArrayAdapter<DetailedData> {
+    ArrayList<DetailedData> list;
+
+
+    public HomeAdapter(Context context, ArrayList<DetailedData> users) {
+        super(context, 0, users);
+        list = users;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        DetailedData data = list.get(position);
+        if(convertView == null)
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.home_textviews, parent, false);
+
+
+        TextView Name = convertView.findViewById(R.id.itemName);
+        Name.setText(data.getFoodType());
+        return convertView;
+    }
+
+
+    public void refreshData(ArrayList<DetailedData> data){
+        this.list.clear();
+        this.list.addAll(data);
+        notifyDataSetChanged();
+    }
+}
